4ed434682b64c4be294919d5528830a5a8f73a23
Sadie Sial <sadiesial@gmail.com>
2021-08-25 18:02:09 -0500

    edited JS pathway link in HTML whoopsie + already having a hard time with the JS displaying hehe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 // assigning global variabels
+var calArea = document.querySelector('#calendar');
 
 // trying to make the header have date + time
+// idk why I'm so nervous to use JQuery lolz
 function dailyCalendar() {
 $(document).ready(function () {
-    var currentDate = moment().format('LTS');
+    var currentDate = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
 })
 
 
