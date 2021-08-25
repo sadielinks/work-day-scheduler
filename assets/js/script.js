cbfd821f965f856d628f18904de35436f65af027
Sadie Sial <sadiesial@gmail.com>
2021-08-25 17:33:03 -0500

    edited HTML to have jquery library link + started JS lol trying to...

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// assigning global variabels
+
+// trying to make the header have date + time
+function dailyCalendar() {
+$(document).ready(function () {
+    var currentDate = moment().format('LTS');
+})
+
+
+
+}
