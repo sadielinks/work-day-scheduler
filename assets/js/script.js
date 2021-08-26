16bcac0304ed8779a8235b35fb4177748e593120
Sadie Sial <sadiesial@gmail.com>
2021-08-25 21:58:56 -0500

    added calendar area via bootstrap, which is VERY convenient

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,39 +5,65 @@ var calArea = document.querySelector('#calendar');
 // idk why I'm so nervous to use JQuery lolz
 // copied currentDate code direct from moment.js
 function dailyCalendar() {
-$(document).ready(function () {
-    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $(document).ready(function () {
+        var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-    // display the date in the header
-    function currentDay() {
-        var currentDay = $('#currentDay').text(currentDate);
-        return currentDay;
-    }
+        // display the date in the header
+        function currentDay() {
+            var currentDay = $('#currentDay').text(currentDate);
+            return currentDay;
+        }
 
-    // hoping to update the time as user on page...?...!
-    function currentTime() {
-        setInterval(function () {
-            currentDate();
-        }, 1000)
-    }
+        // hoping to update the time as user on page...?...!
+        function currentTime() {
+            setInterval(function () {
+                currentDate();
+            }, 1000)
+        }
 
-    currentDay()
+        currentDay()
 
-// now we are going to start *trying* to display the calendar
-// var timeRow;
-// var timeColumn;
-var calHours = []
+        // now we are going to start *trying* to display the calendar
+        // var timeRow;
+        // var timeColumn;
+        var calHours = [
+            '6 am',
+            '7 am',
+            '8 am',
+            '9 am',
+            '10 am',
+            '11 am',
+            '12 pm',
+            '1 pm',
+            '2 pm',
+            '3 pm',
+            '4 pm',
+            '5 pm',
+            '6 pm'
+        ]
 
-// create a loop for the timeblocks
-    for (i = 0; i < calHours.length; i++) {
-        var timeBlock = $('div').addClass('timeRow');
-        var timeRow = 
-    }
+        // have the JQuery + HTML shake hands by connecting the class 'container'
+        function mainCalInBody() {
+            var calContainer = $('.container');
 
 
+            // create a loop for the timeblocks with bootstrap
+            for (i = 0; i < calHours.length; i++) {
+                var timeBlock = $('<div>').addClass('row time-block"');
+                var timeRow = $('<div>').addClass('col-md-1 hour d-flex justify-content-end pt-3');
+                var eventText = $('<textarea>').addClass('event col-md-10');
+                var saveBtn = $('<button>').addClass('saveBtn cold-md-1')
 
-})}
 
+                timeBlock.append(timeRow, eventText, saveBtn);
+                mainCalInBody.append(timeBlock);
+
+
+            } mainCalInBody();
+        }
+
+    })
+}
 
 
 dailyCalendar();
