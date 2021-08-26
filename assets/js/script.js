8b17920c7294d2c1e0c55534cd3925e7d1b471f8
Sadie Sial <sadiesial@gmail.com>
2021-08-25 19:58:08 -0500

    atrying to have the displayed time update as the user is on the page + added some more comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,27 @@ function dailyCalendar() {
 $(document).ready(function () {
     var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 
+    // display the date in the header
     function currentDay() {
         var currentDay = $("#currentDay").text(currentDate);
         return currentDay;
     }
 
-    currentDay()
-    var displayRow;
-    var displayColumn;
+    // hoping to update the time as user on page...?...!
+    function currentTime() {
+        setInterval(function () {
+            currentDate();
+        }, 1000)
+    }
 
+    currentDay()
+
+// now we are going to start *trying* to display the calendar
+var displayRow;
+var displayColumn;
+
+// create a loop for the timeblocks
+    
 
 })}
 
