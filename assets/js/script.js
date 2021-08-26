8435658c107d3b92a83b112cab0794d27b60aa82
Sadie Sial <sadiesial@gmail.com>
2021-08-25 19:01:19 -0500

    maybe I added the current date and time to the jumbotron... maybe not lol maybe not tbh

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,18 @@ var calArea = document.querySelector('#calendar');
 
 // trying to make the header have date + time
 // idk why I'm so nervous to use JQuery lolz
+// copied currentDate code direct from moment.js
 function dailyCalendar() {
 $(document).ready(function () {
-    var currentDate = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-})
+    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    function currentDay() {
+        var currentDay = $("#currentDay").text(currentDate);
+        return currentDay;
+    }
+
+})}
 
 
 
-}
+dailyCalendar();
