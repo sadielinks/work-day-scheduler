1ac0608ef4561545b45661005ec8896b28cf19a7
Sadie Sial <sadiesial@gmail.com>
2021-08-27 17:06:26 -0500

    worked with tutor to find typo that was blocking the coloring from showing- phew

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 // Shows the date + time via moment.js, bless it
 function dailyCalendar() {
     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-        } 
+}
 //callin it bb
 dailyCalendar();
 
@@ -18,24 +18,6 @@ function currentTime() {
 // callin it bb
 currentTime()
 
-// now we are going to start *trying* to display the calendar
-// var timeRow;
-// var timeColumn;
-// var calHours = [
-//     '6 am',
-//     '7 am',
-//     '8 am',
-//     '9 am',
-//     '10 am',
-//     '11 am',
-//     '12 pm',
-//     '1 pm',
-//     '2 pm',
-//     '3 pm',
-//     '4 pm',
-//     '5 pm',
-//     '6 pm'
-// ]
 
 
 // have the JQuery + HTML shake hands by connecting the class 'container'
@@ -44,17 +26,24 @@ function mainCalBody() {
     var calContainer = $('.container');
     // create a loop for the timeblocks with bootstrap
     for (i = 6; i < 20; i++) {
-        var timeBlock = $('<div>').addClass('row time-block"');
+        var timeBlock = $('<div>').addClass('row time-block');
         var timeRow = $('<div>').addClass('col-md-1 hour d-flex justify-content-end pt-3');
-        var textArea = $('<textarea>').addClass('event col-md-10');
-        var saveBtn = $('<button>').addClass('saveBtn cold-md-1');
+        var textArea = $('<textarea>').addClass('col-md-10 description');
+        var saveBtn = $('<button>').addClass('saveBtn col-md-1');
+        // save icon from font awesome
 
-        timeBlock.attr("id", i);
+        
+        // template literals <3
+        timeRow.text(`${i}:00`);
+        timeBlock.attr('id', `hour-${i}`);
+
         timeBlock.append(timeRow, textArea, saveBtn);
         calContainer.append(timeBlock);
 
         // need to figure out how to have the times appear... hmmm
     }
+    // callin it baby
+    colorScheduleBlocks();
 }
 // calling it bb
 mainCalBody();
@@ -62,23 +51,24 @@ mainCalBody();
 // Now to show the schedule blocks according to time of day
 function colorScheduleBlocks() {
     // parseInt to make the moment argument into a string
-    var hour = parseInt(moment().format('HH'));
+    var momentHour = moment().hours();
     // connect to timeblock loop code from earlier
     $('.time-block').each(function () {
         // BCS LA suggested using .this but I'm not able to see the blocks... heh
-        var thisHour = parseInt($(this).attr("id"));
-    if (thisHour < hour) {
-      $(this).addClass("past");
-    } else if (thisHour === hour) {
-      $(this).addClass("present");
-    } else {
-      $(this).addClass("future");
-    }
+        var thisHour = parseInt($(this).attr('id').split('-')[1]);
+        if (thisHour < momentHour) {
+            $(this).addClass('past');
+        } else if (thisHour === momentHour) {
+            $(this).removeClass('past')
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+            $(this).removeClass('present')
+            $(this).removeClass('past')
+        }
     })
 }
-// callin it baby
-colorScheduleBlocks();
 
 
 
-// local storage components 
+// local storage components - unique ids to
